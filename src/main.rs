df71e99e5c11cd47e4711dcdb5a12c23907c2a4a
Hyeon Kim <simnalamburt@gmail.com>
2023-06-17 05:09:45 +0900

    Replace async recursion with walkdir

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use std::path::PathBuf;
+use std::borrow::Cow;
+use std::fs::rename;
+use std::path::{Component, Path, PathBuf};
+use std::thread;
+use std::time::Instant;
 
-use async_recursion::async_recursion;
 use clap::Parser;
+use crossbeam_channel::unbounded;
 use rlimit::Resource;
-use tokio::{sync::Semaphore, time::Instant};
-use unicode_normalization::{is_nfc, UnicodeNormalization};
+use walkdir::WalkDir;
 
-const CONCURRENT_TASKS: usize = 32768;
-static SEMA: Semaphore = Semaphore::const_new(CONCURRENT_TASKS);
-
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(version, arg_required_else_help = true)]
 struct Args {
     #[arg(long)]
-    follow_directory_symlinks: bool,
+    follow_symlinks: bool,
     #[arg(short, long)]
     verbose: bool,
     #[arg(short = 'n', long)]
@@ -23,93 +23,105 @@ struct Args {
     paths: Vec<PathBuf>,
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let args = Args::parse();
 
     // Automatically increase NOFILE rlimit to the allowed maximum
-    rlimit::increase_nofile_limit(u64::MAX).expect("failed during increasing NOFILE rlimit");
+    if let Err(e) = rlimit::increase_nofile_limit(u64::MAX) {
+        eprintln!("warning: failed to increase NOFILE rlimit ({e})");
+    }
 
     let nofile = rlimit::getrlimit(Resource::NOFILE).expect("cannot query rlimit");
     if nofile.0 < 1024 || nofile.1 < 1024 {
         eprintln!("warning: NOFILE resource limit is low(={nofile:?}), run `ulimit -n 65536` and try again if panic occurs");
     }
 
-    if args.follow_directory_symlinks {
-        eprintln!("warning: --follow_directory_symlinks is ON; be aware for infinite recursion!");
-    }
-
     let start = Instant::now();
 
-    let cnt = normalize_paths(
-        args.follow_directory_symlinks,
-        args.verbose,
-        args.dry_run,
-        args.paths.clone(),
-    )
-    .await;
+    let (tx, rx) = unbounded::<PathBuf>();
+
+    let thread_join_handle = thread::spawn(move || {
+        let mut success = 0usize;
+        let mut error = 0usize;
+        while let Ok(path) = rx.recv() {
+            let mut it = path.components();
+            let Some(Component::Normal(oldname)) = it.next_back() else { continue }; // skip '..' or '/'
+            let Some(oldname) = oldname.to_str() else { continue }; // skip non-unicode filename
+            let Cow::Owned(newname) = normalize_into_nfc(oldname) else { continue }; // alloc or skip NFC
+
+            let dirname = it.as_path();
+            let Some(dirname) = dirname.to_str() else { continue }; // skip non-unicode dirname
+
+            // NOTE: Actual dirname will be already NFC at this point but this thread can still see
+            // NFD dirname. This is because directory traversal and renaming are done in separate
+            // threads. So we should NFC-normalize the dirname to avoid TOCTOU.
+            let dirname = normalize_into_nfc(dirname); // maybe alloc
+            let dirname = Path::new(&*dirname);
+
+            let old = dirname.join(oldname); // alloc
+            let new = dirname.join(newname); // alloc
+
+            if args.dry_run {
+                success += 1;
+                eprintln!("dryrun: {old:?} -> {new:?}");
+                continue;
+            }
+            if let Err(e) = rename(&old, &new) {
+                error += 1;
+                eprintln!("error: {old:?} -> {new:?} failed with {e}");
+                continue;
+            }
+            success += 1;
+            if args.verbose {
+                eprintln!("success: {old:?} -> {new:?}");
+            }
+        }
+        (success, error)
+    });
+
+    for p in args.paths {
+        for entry in WalkDir::new(p).follow_links(args.follow_symlinks) {
+            let Ok(entry) = entry else {
+                eprintln!("skip: {}", entry.unwrap_err());
+                continue;
+            };
+            let path = entry.into_path();
+            tx.send(path).unwrap(); // never fails
+        }
+    }
+    drop(tx);
+    let (success, error) = thread_join_handle
+        .join()
+        .expect("thread panicked unexpectedly");
 
     let elapsed = start.elapsed();
-
-    eprintln!("DONE; {cnt} files in {} seconds", elapsed.as_secs_f64());
+    if args.dry_run {
+        eprintln!("{success} files will be renamed, took {elapsed:?}");
+    } else {
+        eprintln!("renamed {success} files, took {elapsed:?} seconds");
+        if error > 0 {
+            eprintln!("failed to rename {error} files");
+            std::process::exit(1);
+        }
+    }
 }
 
-#[async_recursion]
-async fn normalize_paths(
-    follow_symlinks: bool,
-    verbose: bool,
-    dry_run: bool,
-    paths: Vec<PathBuf>,
-) -> u64 {
-    let mut tasks = Vec::new();
-    let mut cnt = 0;
-    for p in paths {
-        let _aq = SEMA.acquire();
-        let task = tokio::task::spawn(async move {
-            let mut cnt = 0;
-            let mut p = if let Some(p) = p.to_str().map(str::to_owned) {
-                p
+/// Return a string normalized in NFC. If the string is already in NFC, it is returned as-is.
+///
+/// This function is required to reduce repeated computation and memory allocation.
+fn normalize_into_nfc(s: &str) -> Cow<str> {
+    use unicode_normalization::{is_nfc_quick, IsNormalized, UnicodeNormalization};
+
+    match is_nfc_quick(s.chars()) {
+        IsNormalized::Yes => s.into(),
+        IsNormalized::No => s.nfc().collect(),
+        IsNormalized::Maybe => {
+            let nfc: String = s.nfc().collect();
+            if s == nfc {
+                s.into()
             } else {
-                eprintln!("warning: {} is not a valid UTF-8 path", p.to_string_lossy());
-                return 0;
-            };
-
-            if !is_nfc(&p) {
-                let newp = p.nfc().collect::<String>();
-                if !dry_run {
-                    match tokio::fs::rename(&p, &newp).await {
-                        Ok(()) => {
-                            if verbose {
-                                eprintln!("success: {p} -> {newp}");
-                            }
-                            cnt += 1;
-                        }
-                        Err(e) => eprintln!("failure: {p} -> {newp}: {e}"),
-                    }
-                } else {
-                    eprintln!("skip: {p} -> {newp}");
-                }
-                p = newp;
+                nfc.into()
             }
-
-            let p = PathBuf::from(p);
-            if (!p.is_symlink() || follow_symlinks) && p.is_dir() {
-                let mut paths = Vec::new();
-                let mut dir = tokio::fs::read_dir(p).await.expect("cannot list directory");
-                while let Ok(Some(d)) = dir.next_entry().await {
-                    paths.push(d.path());
-                }
-                cnt += normalize_paths(follow_symlinks, verbose, dry_run, paths).await;
-            }
-
-            cnt
-        });
-        tasks.push(task);
+        }
     }
-
-    for t in tasks {
-        cnt += t.await.unwrap();
-    }
-
-    cnt
 }
